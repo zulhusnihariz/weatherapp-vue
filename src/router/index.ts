4bd07fa58ddb2d695dfe4c8abf1d7232903b2395
zulhusnihariz <zulhusnihariz97@gmail.com>
2024-12-20 04:10:42 +0800

    feat(route): setup page routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
+import HomeView from '@/views/HomeView.vue'
+
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +8,20 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/profile',
+      name: 'profile',
+      component: () => import("@/views/ProfileView.vue")
     },
-  ],
+    {
+      path: '/weather/:id',
+      name: 'weather',
+      component: () => import("@/views/WeatherDetailView.vue"),
+    },
+  ]
+  ,
 })
 
 export default router
