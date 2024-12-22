16fc20d4c868746dc28d3d2c53c2e2c3574eac5a
zulhusnihariz <zulhusnihariz97@gmail.com>
2024-12-22 18:39:14 +0800

    chore: update `eslint.config.js`

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,27 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
 
 export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      'vue/multi-word-component-names': [
+        'error',
+        {
+          ignores: ['default'],
+        },
+      ],
+
+      'vue/no-unused-vars': [
+        'error',
+        {
+          ignores: ['default'],
+        },
+      ],
+    },
   },
 
   {
@@ -16,7 +31,7 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
