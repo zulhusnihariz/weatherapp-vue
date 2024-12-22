27d0c20f88175f9933202aa8a03243fc0b462b23
zulhusnihariz <zulhusnihariz97@gmail.com>
2024-12-22 18:29:59 +0800

    refactor: remove navigation bar from `App.vue`

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { RouterView, useRoute } from 'vue-router'
-import HomeNavbar from './navbar/HomeNavbar.vue'
-import ProfileNavbar from './navbar/ProfileNavbar.vue'
-
-const route = useRoute()
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <ProfileNavbar v-if="route.path == '/profile'" />
-  <HomeNavbar v-if="route.path == '/'" />
-  <RouterView />
+  <Suspense>
+    <RouterView />
+  </Suspense>
 </template>
