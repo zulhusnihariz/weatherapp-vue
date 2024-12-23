12406bbeee369ca5f9b1fb84f4f523a554b29946
zulhusnihariz <zulhusnihariz97@gmail.com>
2024-12-24 00:51:47 +0800

    build(deps): install prima-vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import 'primeicons/primeicons.css'
 import './assets/main.css'
 
+import PrimaVue from 'primevue/config'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +9,9 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimaVue, {
+  unstyled: true
+}
+)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({ resolvers: [PrimeVueResolver()] })
   ],
   resolve: {
     alias: {
