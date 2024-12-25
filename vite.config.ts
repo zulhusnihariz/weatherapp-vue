043fae7edb4a97dfb481d30c63efcff2c8c7f47b
zulhusnihariz <zulhusnihariz97@gmail.com>
2024-12-26 04:22:37 +0800

    chore: add base in `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/weatherapp-vue',
   plugins: [
     vue(),
     vueDevTools(),
