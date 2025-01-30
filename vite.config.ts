b6e991bcbc04c62943a50ee28c6482671cf0fd2c
zulhusnihariz <zulhusnihariz97@gmail.com>
2025-01-30 20:20:02 +0800

    fix(build): blank white page

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weatherapp-vue',
+  base: '',
   plugins: [
     vue(),
     vueDevTools(),
